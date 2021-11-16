39ce3bb1413d14c11e12244e482239f2dbe1d1a3
yazanabdulhafez <yazanali31@yahoo.com>
2021-11-16 15:03:17 +0200

    finish lab 28

--- app/src/main/java/com/example/taskmaster/Activities/TaskDetail.java
+++ app/src/main/java/com/example/taskmaster/Activities/TaskDetail.java
@@ -1,4 +1,4 @@
-package com.example.taskmaster;
+package com.example.taskmaster.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.taskmaster.R;
 
 public class TaskDetail extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/taskmaster/Adapters/TaskAdapter.java
@@ -0,0 +1,78 @@
+package com.example.taskmaster.Adapters;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.taskmaster.Activities.MainActivity;
+import com.example.taskmaster.Activities.TaskDetail;
+import com.example.taskmaster.Models.Task;
+import com.example.taskmaster.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+
+    List<Task> allTasks=new ArrayList<Task>();
+
+    public TaskAdapter(List<Task> allTasks) {
+        this.allTasks = allTasks;
+    }
+
+
+
+    public static class TaskViewHolder extends RecyclerView.ViewHolder{
+
+        public Task task;
+        public View itemView;
+
+        public TaskViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView=itemView;
+        }
+    }
+
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_task,parent,false);
+        TaskViewHolder taskViewHolder=new TaskViewHolder(view);
+        return taskViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        holder.task=allTasks.get(position);
+        TextView title=holder.itemView.findViewById(R.id.titleTextView);
+        TextView body=holder.itemView.findViewById(R.id.bodyTextView);
+        TextView state=holder.itemView.findViewById(R.id.stateTextView);
+        Button gotodetails =holder.itemView.findViewById(R.id.goToDetailsButton);
+
+        title.setText(holder.task.getTitle());
+        body.setText(holder.task.getBody());
+        state.setText(holder.task.getState());
+        gotodetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String taskTitle=holder.task.getTitle();
+                Intent goToDetailsPage=new Intent(v.getContext(),TaskDetail.class);
+                goToDetailsPage.putExtra("taskTitle",taskTitle);
+                v.getContext().startActivity(goToDetailsPage);
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return allTasks.size();
+    }
+}
